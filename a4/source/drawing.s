e803c227e184291069eb7e90e8102544915cfaaa
danielnwaroh <daniel.nwaroh@gmail.com>
2018-03-27 16:47:16 -0600

    Different files implemented. Only background img

--- a4/source/drawing.s
+++ a4/source/drawing.s
@@ -3,19 +3,40 @@
 @ Code section
 .section .text
 
-/*
+		
+@ Draw Pixel
+@ r0 - x
+@ r1 - y
+@ r2 - color		
+
+.global DrawPixel
+DrawPixel:
+	push	{r4, r5}
+	offset	.req	r4
+	ldr	r5,	=frameBufferInfo
+	ldr	r3,	[r5, #4]
+	mul	r1,	r3
+	add	offset,	r0,	r1
+	lsl		offset, #2
+	ldr	r0,	[r5]
+	str	r2,	[r0, offset]
+	pop		{r4, r5}
+	bx		lr
+
 .global	drawImage
 drawImage:
 		push	{r5-r10, lr}
 		
-		mov	r10,#0
-		
+		mov	r10, r0
 		mov	r5,	#0
 		mov	r6,	#0
-		mov	r7,	#208
-		mov	r8,	#232
+		ldr	r7, =imageWidth
+		ldr	r7, [r7]
+		ldr	r8, =imageHeight
+		ldr	r8, [r8]
+		
 		mov	r9,	r2
-
+		
 outerLoop:
 
 		mov	r5, #0			//reset pixels drawn x
@@ -26,7 +47,7 @@ outerLoop:
 		cmp	r6, r8			//Pixels drawn y < y bound?
 		bge	done1		
 	
-drawLoop:
+drawLoop1:
 		cmp	r5, r7			//Pixels drawn < Length?
 		bge	outerLoop		//If yes, branch to done
 
@@ -39,91 +60,10 @@ drawLoop:
 		add	r5,	r5, #1		//Add one to pixels drawn	
 		add	r0,	r0, #1		//Add one to the x-coordinat
 
-		b	drawLoop
-		
-		
+		b	drawLoop1
 
-done1:
+done1:		
 		pop	{r5-r10, lr}
-		mov	pc,	lr
-		
-.global DrawChars
-DrawChars:
-		push	{r4-r10, lr}
-		
-		chAdr	.req	r4
-		px		.req	r5
-		py		.req	r6
-		row		.req	r7
-		mask	.req	r8
-		pxinit	.req	r9
-		color	.req	r10
-		
-		ldr		chAdr,	=font
-		add		chAdr,	r3,	lsl #4
-		
-		mov		color,	r2
-		mov		py,		r1
-		mov		pxinit,	r0
-		
-charLoop$:
-		mov		px, pxinit		@ init the X coordinate
-
-		mov		mask, #0x01		@ set the bitmask to 1 in the LSB
-		
-		ldrb	row, [chAdr], #1	@ load the row byte, post increment chAdr
-
-rowLoop$:
-		tst		row,	mask		@ test row byte against the bitmask
-		beq		noPixel$
-
-		mov		r0, px
-		mov		r1, py
-		mov		r2, #0x00FF0000		@ red
-		bl		DrawPixel		@ draw red pixel at (px, py)
-
-noPixel$:
-		add		px, #1			@ increment x coordinate by 1
-		lsl		mask, #1		@ shift bitmask left by 1
-
-		tst		mask,	#0x100		@ test if the bitmask has shifted 8 times (test 9th bit)
-		beq		rowLoop$
-
-		add		py, #1			@ increment y coordinate by 1
-
-		tst		chAdr, #0xF
-		bne		charLoop$		@ loop back to charLoop$, unless address evenly divisibly by 16 (ie: at the next char)
-
-		.unreq	chAdr
-		.unreq	px
-		.unreq	py
-		.unreq	row
-		.unreq	mask
-
-		pop		{r4-r8, pc}
-	
-		
-
-
-.global drawMainMenu
-drawMainMenu:
-
-
-
-.global drawGameState
-drawMainMenu:
-
-
-.global drawGameMenu
-drawMainMenu:
-
-
-@ Data section
-.section .data
-
-font:
-.incbin	"font.bin"
-
-*/
+		mov	pc, lr
 
 
--- a4/source/main.s
+++ a4/source/main.s
@@ -20,167 +20,25 @@ main:
 	@ ask for the frame buffer information
 	ldr		r0, =frameBufferInfo		@frame buffer information structure
 	bl		initFbInfo
+
+PrintBackground:
 	
-	mov		r4,	#0
-	mov		r0,	#600
-	mov		r1,	#600
-	//bl		drawLoop
-	b		looper
-drawLoop:
-	cmp		r4,	#300
-	bge		done
-	mov		r8, r0
-	mov		r9, r1
-	ldr		r2,	=0xFF0000	
-	
-	bl		DrawPixel
-		
-	add		r4,	#1
-	add		r8,	#1
-	mov		r0, r8
-	mov		r1, r9
-	bl		drawLoop
-	bx		lr
-done:
-	mov		pc,	lr
-looper:	
-	mov		r0, #200
+	mov		r0, #350
 	mov		r1, #100
+	ldr		r7, =imageWidth
+	mov		r4, #1080
+	str		r4, [r7]
+	ldr		r8, =imageHeight
+	mov		r4, #675
+	str		r4, [r8]
+	ldr		r2, =background
 	bl		drawImage
-	mov		r4,	#0
-	mov		r0,	#600
-	mov		r1,	#600
-	bl		drawLoop
+
+
 haltLoop$:
 		b	haltLoop$
-		
-@ Draw Pixel
-@ r0 - x
-@ r1 - y
-@ r2 - color		
-
-.global DrawPixel
-DrawPixel:
-	push	{r4, r5}
-	
-	
-
-	offset	.req	r4
-		
-	ldr	r5,	=frameBufferInfo
-	
-	ldr	r3,	[r5, #4]
-	mul	r1,	r3
-	add	offset,	r0,	r1
-
-	lsl		offset, #2
-
-	ldr	r0,	[r5]
-	str	r2,	[r0, offset]
-
-	pop		{r4, r5}
-	bx		lr
-/*	
-.global Draw
-Draw:
-	push		{r4-r9, lr}
-	mov r5, #0	// r5 is count x
-	mov r8, #0	// r8 is count y
-	
-	ldr	r4, =background
-	mov r6, #500	// x is r6
-	mov r7, #500	// y is r7
-
-loopcol:
-
-	
-looprow:
-	mov r0, r6
-	mov r1, r7
-	ldr	r2, [r4]
-	//mov	r2, #0x00FF0000	
-	bl DrawPixel
-	
-	add r5, #1
-	add r6, #1
-	//add r7, r8
-	add r4, #4
-	cmp	r5, #15
-	ble	looprow
-	mov r6, #500
-	mov	r5, #0
-	
-	cmp r8, #15
-	addlt r8, #1
-	addlt r7, #1
-	blt loopcol
-
-	pop		{r4-r9, pc}
-*/
 
 
-
-
-
-
-
-
-
-.global	drawImage
-drawImage:
-		push	{r5-r10, lr}
-		
-		mov	r10, r0
-		//mov	r10,#200
-		
-		mov	r5,	#0
-		mov	r6,	#0
-		mov	r7,	#1080
-		mov	r8,	#675
-		ldr	r2, =background
-		mov	r9,	r2
-		
-outerLoop:
-
-		mov	r5, #0			//reset pixels drawn x
-		mov	r0, r10		//Re-initialize x -coordinate
-		add	r1, r1,	#1		//Add one to y coordinate
-		add	r6, r6,	#1		//Add one to pixels drawn y
-
-		cmp	r6, r8			//Pixels drawn y < y bound?
-		bge	done1		
-	
-drawLoop1:
-		cmp	r5, r7			//Pixels drawn < Length?
-		bge	outerLoop		//If yes, branch to done
-
-		ldr	r2, [r9], #4		
-
-		push	{r0, r1, r2}
-		bl	DrawPixel
-		pop	{r0, r1, r2}			
-		
-		add	r5,	r5, #1		//Add one to pixels drawn	
-		add	r0,	r0, #1		//Add one to the x-coordinat
-
-		b	drawLoop1
-
-done1:		
-		pop	{r5-r10, lr}
-		mov	pc, lr
-
-
-
-
-
-
-
-
-
-
-
-		
-		
 
 @ Data section
 .section .data
@@ -201,6 +59,14 @@ FrameBuffer:
 @For drawing ASCII TEXT
 .align 4
 font:	.incbin "font.bin"
+
+.global imageWidth
+imageWidth:
+.int	0
+.global	imageHeight
+imageHeight:
+.int	0
+
 
 
 
