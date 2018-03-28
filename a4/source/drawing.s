8edeae20e607ab6573710c81f9c07bbbc1f5f528
issackj1 <issack.john1@ucalgary.ca>
2018-03-27 18:02:29 -0600

    background and borders drawing

--- a4/source/drawing.s
+++ a4/source/drawing.s
@@ -3,67 +3,181 @@
 @ Code section
 .section .text
 
+
+.global drawLeftBorder
+drawLeftBorder:
+	push	{r4-r9, lr}
+	x	.req	r4
+	y	.req	r5
 		
+	mov		x,	#500
+	mov		y,	#500
+	
+	ldr		r6,	=leftBorder
+	mov		r7,	#0
+	mov		r8,	#0
+	
+loop:
+	mov		r0,	x
+	mov		r1,	y
+	ldr		r2,	[r6]
+	bl		DrawPixel
+	
+	add		x,	#1
+	add		r6,	#4
+	add		r7,	#1
+	
+	cmp		r7,	#6
+	blt		loop
+	mov		x,	#500
+	mov		r7, #0
+	
+	add		r8,	#1
+	add		y,	#1
+	cmp 	r8, #230
+	blt 	loop
+
+	pop		{r4-r9, pc}
+
+
+.global drawRightBorder
+drawRightBorder:
+	push	{r4-r9, lr}
+	x	.req	r4
+	y	.req	r5
+		
+	mov		x,	#714
+	mov		y,	#500
+	
+	ldr		r6,	=rightBorder
+	mov		r7,	#0
+	mov		r8,	#0
+	
+loopr:
+	mov		r0,	x
+	mov		r1,	y
+	ldr		r2,	[r6]
+	bl		DrawPixel
+	
+	add		x,	#1
+	add		r6,	#4
+	add		r7,	#1
+	
+	cmp		r7,	#6
+	blt		loopr
+	mov		x,	#714
+	mov		r7, #0
+	
+	add		r8,	#1
+	add		y,	#1
+	cmp 	r8, #230
+	blt 	loopr
+
+	pop		{r4-r9, pc}
+
+
+.global drawTopBorder
+drawTopBorder:
+	push	{r4-r9, lr}
+	x	.req	r4
+	y	.req	r5
+		
+	mov		x,	#500
+	mov		y,	#500
+	
+	ldr		r6,	=topBorder
+	mov		r7,	#0
+	mov		r8,	#0
+	
+loopt:
+	mov		r0,	x
+	mov		r1,	y
+	ldr		r2,	[r6]
+	bl		DrawPixel
+	
+	add		x,	#1
+	add		r6,	#4
+	add		r7,	#1
+	
+	cmp		r7,	#220
+	blt		loopt
+	mov		x,	#500
+	mov		r7, #0
+	
+	add		r8,	#1
+	add		y,	#1
+	cmp 	r8, #7
+	blt 	loopt
+
+	pop		{r4-r9, pc}
+
+
+.global drawBackGround
+drawBackGround:
+	push		{r4-r9, lr}
+	mov 	r5, #0	// r5 is count x
+	mov 	r8, #0	// r8 is count y
+	
+	ldr		r4, =background
+	mov 	r6, #500	// x is r6
+	mov 	r7, #500	// y is r7
+
+loopcol:
+
+	
+looprow:
+	mov 	r0, r6
+	mov 	r1, r7
+	ldr		r2, [r4]
+	//mov	r2, #0x00FF0000	
+	bl 		DrawPixel
+	
+	add 	r5, #1
+	add 	r6, #1
+	//add r7, r8
+	add 	r4, #4
+	cmp		r5, #208
+	blt		looprow
+	mov 	r6, #500
+	mov		r5, #0
+	
+	cmp 	r8, #232
+	addlt 	r8, #1
+	addlt 	r7, #1
+	blt 	loopcol
+
+	pop		{r4-r9, pc}
+	
 @ Draw Pixel
 @ r0 - x
 @ r1 - y
 @ r2 - color		
-
-.global DrawPixel
+.global DrawPixel		
 DrawPixel:
-	push	{r4, r5}
-	offset	.req	r4
-	ldr	r5,	=frameBufferInfo
-	ldr	r3,	[r5, #4]
-	mul	r1,	r3
-	add	offset,	r0,	r1
-	lsl		offset, #2
-	ldr	r0,	[r5]
-	str	r2,	[r0, offset]
-	pop		{r4, r5}
-	bx		lr
+		push	{r4,r5}
+		offset	.req	r4
 
-.global	drawImage
-drawImage:
-		push	{r5-r10, lr}
-		
-		mov	r10, r0
-		mov	r5,	#0
-		mov	r6,	#0
-		ldr	r7, =imageWidth
-		ldr	r7, [r7]
-		ldr	r8, =imageHeight
-		ldr	r8, [r8]
-		
-		mov	r9,	r2
-		
-outerLoop:
-
-		mov	r5, #0			//reset pixels drawn x
-		mov	r0, r10		//Re-initialize x -coordinate
-		add	r1, r1,	#1		//Add one to y coordinate
-		add	r6, r6,	#1		//Add one to pixels drawn y
-
-		cmp	r6, r8			//Pixels drawn y < y bound?
-		bge	done1		
+		ldr		r5, =frameBufferInfo
 	
-drawLoop1:
-		cmp	r5, r7			//Pixels drawn < Length?
-		bge	outerLoop		//If yes, branch to done
-
-		ldr	r2, [r9], #4		
-
-		push	{r0, r1, r2}
-		bl	DrawPixel
-		pop	{r0, r1, r2}			
+		@offset = (y * width) + x
+	
+		ldr		r3, [r5, #4]			@ r3 = width
+		mul		r1, r3
+		add		offset, r0, r1
 		
-		add	r5,	r5, #1		//Add one to pixels drawn	
-		add	r0,	r0, #1		//Add one to the x-coordinat
+		@ offset *= 4 (32 bits per pixel/8 = 4 bytes per pixel)
+		lsl		offset, #2
+		
+		@ store the color (word) at frame buffer pointer + offset
+		ldr		r0, [r5]				@ r0 = frame buffer pointer
+		str		r2, [r0, offset]
+		
+		pop		{r4, r5}
+		bx		lr
 
-		b	drawLoop1
+@ Data section
+.section .data
 
-done1:		
-		pop	{r5-r10, lr}
-		mov	pc, lr
+
 
 
--- a4/source/main.s
+++ a4/source/main.s
@@ -20,25 +20,18 @@ main:
 	@ ask for the frame buffer information
 	ldr		r0, =frameBufferInfo		@frame buffer information structure
 	bl		initFbInfo
-
-PrintBackground:
 	
-	mov		r0, #350
-	mov		r1, #100
-	ldr		r7, =imageWidth
-	mov		r4, #1080
-	str		r4, [r7]
-	ldr		r8, =imageHeight
-	mov		r4, #675
-	str		r4, [r8]
-	ldr		r2, =background
-	bl		drawImage
+	bl		drawBackGround
+	bl		drawLeftBorder
+	bl		drawRightBorder
+	bl		drawTopBorder
+	
+	
+	
+	haltLoop$:
 
-
-haltLoop$:
 		b	haltLoop$
-
-
+			
 
 @ Data section
 .section .data
@@ -50,24 +43,9 @@ frameBufferInfo:
 .int	0
 .int	0
 
-.global	FrameBuffer
-FrameBuffer:
-.int	0
-.int	0
-.int	0
-
 @For drawing ASCII TEXT
 .align 4
 font:	.incbin "font.bin"
-
-.global imageWidth
-imageWidth:
-.int	0
-.global	imageHeight
-imageHeight:
-.int	0
-
-
 
 
 
