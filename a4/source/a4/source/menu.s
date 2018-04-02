8138b046d3443d4d03b086fb2c2b71edde41698e
issackj1 <issack.john1@ucalgary.ca>
2018-04-02 07:49:35 -0600

    Add files via upload

---
+++ a4/source/a4/source/menu.s
@@ -0,0 +1,67 @@
+@ Code section
+.section .text
+
+.global	DrawMenuStartSelected
+DrawMenuStartSelected:
+		push 	{lr}
+	
+		mov	r0,	#640
+		mov	r1,	#200
+		ldr	r8, =imageWidth
+		mov	r7, #640
+		str	r7, [r8]
+		ldr	r8, =imageHeight
+		mov	r6, #360
+		str	r6, [r8]
+		ldr	r2, =startScreen
+		bl	drawImage
+		//bl	SNESmain
+		//b	StartChecker
+		
+		pop	{pc}
+		mov	pc,	lr
+		
+.global	DrawMenuQuitSelected
+DrawMenuQuitSelected:
+		push 	{lr}
+	
+		mov	r0,	#640
+		mov	r1,	#200
+		ldr	r8, =imageWidth
+		mov	r7, #640
+		str	r7, [r8]
+		ldr	r8, =imageHeight
+		mov	r6, #360
+		str	r6, [r8]
+		ldr	r2, =startQuit
+		bl	drawImage
+		//bl	SNESmain
+		//b	StartChecker
+		
+		pop	{pc}
+		mov	pc,	lr
+		
+.global DrawYouLose
+DrawYouLose:
+		push	{lr}
+		
+		mov	r0,	#640
+		mov	r1,	#100
+		ldr	r8, =imageWidth
+		mov	r7, #640
+		str	r7, [r8]
+		ldr	r8, =imageHeight
+		mov	r6, #427
+		str	r6, [r8]
+		ldr	r2, =uLose
+		bl	drawImage
+		
+		pop		{pc}
+
+
+@ Data section
+.section .data
+
+.global MainMenu
+MainMenu:
+.asciz	"Main Menu"
